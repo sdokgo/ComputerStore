48a5ca90132d23da641096d6d164b321105d8659
Dokgo <deathlife.2k@gmail.com>
2021-10-09 11:58:45 +0700

    BinhBH: init project

---
+++ app/src/main/java/com/huybinh2k/computerstore/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.huybinh2k.computerstore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.huybinh2k.computerstore.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
